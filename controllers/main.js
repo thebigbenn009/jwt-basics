2de28ef7018e36e624c10e2cea40d68c872abdef
Benedict <aliubenedict@gmail.com>
2024-03-17 19:08:07 +0100

    first commit-h

--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,25 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 const login = async (req, res) => {
   const { username, password } = req.body;
   console.log(username, password);
-  res.send("Fake Login/Register/Signup Route");
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide a username or password", 400);
+  }
+  //just for practie, normally ptovided by the database
+  const id = new Date().getDate();
+  //try to keep payload small. it is a better experience for the user.
+  //just for demo. In production, use long, complex and unguessable string value.
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
 };
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: "Hello, John Doe",
